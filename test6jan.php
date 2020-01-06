452a51827f76e8047d3d715b51e1fcc21c3b93ee
slintelman <slintelman@olympic.edu>
2020-01-06 07:07:26 -0800

    Create test6jan.php

---
+++ test6jan.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<h1>some math in php</h1>
+	<?php
+		$a = 3;
+		$b = 4;
+		$c = sqrt(pow($a, 2) + pow($b, 2));
+		echo "c = " . $c;
+	?>
+</body>
+</html>
